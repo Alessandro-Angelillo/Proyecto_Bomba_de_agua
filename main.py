449033dd9c76f1b15e1b30ba0184480848198129
Alessandro Angelillo <alessandroangelillo4@gmail.com>
2025-08-01 16:06:42 -0300

    Se agrega la logica de prendido y apagado de la bomba

--- main.py
+++ main.py
@@ -3,7 +3,18 @@ import utime
 
 print("Hello, ESP32!")
 
+##########################################
+#         Definicion de valores          #
+##########################################
 
+Largo_tanque = 100    #Medida de la altura del tanque en cm
+limite_superior = 10*Largo_tanque/100  # En el cual la bomba deja de funcionar (desde que baja un 10%)
+limite_inferior = 60*Largo_tanque/100  # En el cual la bomba empieza a funcionar (desde que baja un 60%)
+estado_de_la_bomba = False
+
+##########################################
+#        Definicion de funciones         #
+##########################################
 trig = Pin(5, Pin.OUT)
 echo = Pin(18, Pin.IN)
 led_bomba = Pin(2, Pin.OUT)
@@ -25,7 +36,28 @@ def medir_distancia():
     distancia_cm = (duracion / 2) / 29.155
     return distancia_cm
 
+def controlar_bomba(distancia):  # Funcion para prender/apagar bomba dependiendo del nivel del tanque
+    global estado_de_la_bomba
+    if distancia > limite_inferior:
+        estado_de_la_bomba = True
+    elif distancia < limite_superior:
+        estado_de_la_bomba = False
+
+def imprimir_estado():  # Funcion para ver por la terminal si la bomba esta prendida o apagada
+    global estado_de_la_bomba
+    if estado_de_la_bomba:
+        print("La bomba esta prendida")
+    else:
+        print("La bomba esta apagada")
+
+##########################################
+#    Comienzo del programa principal     #
+##########################################
+
 while True:
-    distancia = medir_distancia()
-    print("Distancia: {:.2f} cm".format(distancia))
-    utime.sleep(1)
+    distancia_sensor = medir_distancia()
+    print(f"El tanque esta al {100-(distancia_sensor * 100 / Largo_tanque)}%")
+    controlar_bomba(distancia_sensor)
+    imprimir_estado()
+    utime.sleep(0.5)
+    print("\033[2J\033[H")
